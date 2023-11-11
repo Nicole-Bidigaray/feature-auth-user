0a422341ea3430073afda1a2240f720ea37a6081
Nicole Bidigaray <nicole.bidigaray@profarma.com.br>
2023-11-11 20:33:53 -0300

    Chore: Configs, Feat: Tests, Core, Domain and Infra (Clean Architeture)

---
+++ src/infra/http/controllers/create-question.controller.ts
@@ -0,0 +1,44 @@
+import { Body, Controller, Post, UseGuards } from '@nestjs/common'
+import { CurrentUser } from '@/infra/auth/current-user-decorator'
+import { JwtAuthGuard } from '@/infra/auth/jwt-auth.guard'
+import { UserPayload } from '@/infra/auth/jwt.strategy'
+import { ZodValidationPipe } from '@/infra/http/pipes/zod-validation-pipe'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
+import { z } from 'zod'
+
+const createQuestionBodySchema = z.object({
+  title: z.string(),
+  content: z.string(),
+})
+
+const bodyValidationPipe = new ZodValidationPipe(createQuestionBodySchema)
+
+type CreateQuestionBodySchema = z.infer<typeof createQuestionBodySchema>
+
+@Controller('/questions')
+@UseGuards(JwtAuthGuard)
+export class CreateQuestionController {
+  constructor(private prisma: PrismaService) {}
+
+  @Post()
+  async handle(
+    @Body(bodyValidationPipe) body: CreateQuestionBodySchema,
+    @CurrentUser() user: UserPayload,
+  ) {
+    const { title, content } = body
+    const userId = user.sub
+
+    // const slug = this.convertToSlug(title)
+
+    await this.prisma.question.create({
+      data: {
+        authorId: userId,
+        title,
+        content,
+        slug: 'as',
+      },
+    })
+
+    return 'ok'
+  }
+}
